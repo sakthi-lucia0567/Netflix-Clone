344af81b82d67f576463bec74d89de4ea66d29d1
John Doe <sakthivel8725@gmail.com>
2023-03-25 14:22:48 +0530

    feat: Create the Netflix clone app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  console.log("hello world", a);
+  return (
+    <>
+      {" "}
+      <div>
+        <h1>Hello world</h1>
+      </div>{" "}
+    </>
+  );
+}
+
+export default App;
---
+++ src/firbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8XobFBzghgavJfnU4gDtaAN1R4t3D5Lo",
+  authDomain: "petflix-clone.firebaseapp.com",
+  projectId: "petflix-clone",
+  storageBucket: "petflix-clone.appspot.com",
+  messagingSenderId: "903960305056",
+  appId: "1:903960305056:web:bc19c153333d904c4058c5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
